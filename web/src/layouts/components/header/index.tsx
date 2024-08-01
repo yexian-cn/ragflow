85d2901f9507ce3f11c8a5dbe01fa34745f2c1d1
yexian-cn <yejingfeng.us@gmail.com>
2024-08-01 11:05:52 +0800

    Update index.tsx

--- web/src/layouts/components/header/index.tsx
+++ web/src/layouts/components/header/index.tsx
@@ -27,7 +27,8 @@ const RagHeader = () => {
     () => [
       { path: '/knowledge', name: t('knowledgeBase'), icon: KnowledgeBaseIcon },
       { path: '/chat', name: t('chat'), icon: MessageOutlined },
-      { path: '/flow', name: t('flow'), icon: GraphIcon },
+      { path: '/retrievability', name: t('retrievability'), icon: KnowledgeBaseIcon },
+      {/* { path: '/flow', name: t('flow'), icon: GraphIcon }, */}
       { path: '/file', name: t('fileManager'), icon: FileIcon },
     ],
     [t],
